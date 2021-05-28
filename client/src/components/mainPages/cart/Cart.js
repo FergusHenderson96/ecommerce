203ae7bbeddcae6f0368890e83154d21cac008b8
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-05-28 23:02:46 +0100

    added all files including functionality to sign up, add and remove files and add to cart

---
+++ client/src/components/mainPages/cart/Cart.js
@@ -0,0 +1,48 @@
+import React, {useContext, useState} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import {Link} from 'react-router-dom'
+
+function Cart() {
+    const state = useContext(GlobalState)
+    const [cart] = state.userAPI.cart
+    const [total, setTotal] = useState
+
+    if(cart.length === 0)
+    return <h2 style={{textAlign: "center", fontSize: "5rem"}}>Cart empyt</h2>
+    return (
+        <div>
+            {
+                cart.map(product => (
+
+                <div className="detail cart
+                ">
+                    <img src={product.images.url} alt=""/>
+                        <div className="box-detail">
+                            <h2>{product.title}</h2>
+                            <h3>£{product.price * product.quantity}</h3>
+                            <p>{product.description}</p>
+                            <p>{product.content}</p>
+
+                            <div className="amount">
+                                <button> - </button>
+                                <span>{product.quantity}</span>
+                                <button> + </button>
+                            </div>
+
+                            <div className="delete">X</div>
+                            
+                        </div>
+                </div>
+                ))
+            }
+
+                    <dic className="total">
+                        <h3>Total: £ {total}</h3>
+                        <Link to="#!">Payment</Link>
+                    </dic>
+
+        </div>
+    )
+}
+
+export default Cart
---
+++ client/src/components/mainPages/utils/productItem/ProductItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import BtnRender from './BtnRender'
+
+function ProductItem({product, isAdmin}) {
+    return (
+        <div className="product_card">
+            {
+                isAdmin && <input type="checkbox" checked={product.checked}/>
+            }
+            <img src={product.images.url} alt=""/>
+
+            <div className="product_box">
+                <h2 title={product.title}>{product.title}</h2>
+                <span>£{product.price}</span>
+                <p>{product.description}</p>
+            </div>
+
+            <BtnRender product={product} />
+
+            {/* <div className="row_btn">
+                <Link id="btn_buy" to="#!">Buy</Link>
+                <Link id="btn_view" to={`detail/${product._id}`}>View</Link>
+            </div> */}
+        </div>
+    )
+}
+
+export default ProductItem
